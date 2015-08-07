3fd080da1ae264df8bf91df48d1bbd1d84c98f8f
Danilo Wanner <danilo.wanner@gmail.com>
2015-08-07 15:00:43 +0200

    add stylesheet and style/css-loaders

--- app/main.jsx
+++ app/main.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var SqynetApp = require('./components/SqynetApp.jsx');
 
+var css = require('../build/main.css');
+
 React.render(<SqynetApp/>, document.body);
 console.log('Loaded the sqynet app component and rendered it into document body');
