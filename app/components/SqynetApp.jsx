4be103b03e5acb92fdf94a5ad5cda113b6b23120
Danilo Wanner <danilo.wanner@gmail.com>
2015-08-10 10:36:00 +0200

    added fetch polyfill

--- app/components/SqynetApp.jsx
+++ app/components/SqynetApp.jsx
@@ -1,4 +1,14 @@
 var React = require('react');
+require('whatwg-fetch');
+
+/*window.fetch('http://public-api.sqynet.ch')
+  .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    console.log('parsed json', json)
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  })*/
 
 module.exports = React.createClass({
   render: function () {
