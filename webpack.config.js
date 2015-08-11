e724ce2149477841b3295f0fab2dad32fccec3e1
Danilo Wanner <danilo.wanner@gmail.com>
2015-08-11 16:36:29 +0200

    add react-hot-loader and update config to allow hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: ['webpack/hot/dev-server', './app/main.jsx'],
+  entry: ['webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server', './app/main.jsx'],
   output: {
     path: './build',
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'jsx-loader' },
+      { test: /\.jsx$/, loaders: ['react-hot','jsx-loader'] },
       { test: /\.css$/, loader: 'style-loader!css-loader' }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  devtool: "eval",
+  devServer: {
+    hot: true,
+    contentBase: 'build',
+    stats: { colors: true }
   }
 };
