52fb87038176549763bca494a7f7caee57a1545a
Danilo Wanner <danilo.wanner@gmail.com>
2015-09-23 00:36:50 +0200

    grid refactoring

---
+++ app/components/Grid.jsx
@@ -0,0 +1,112 @@
+var React = require('react/addons');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var AutoResponsive = require('autoresponsive-react');
+var Module = require('./Module.jsx');
+
+
+module.exports = React.createClass({
+
+  getInitialState: function() {
+    return {
+      modules: this.getModules(this.props.modules),
+      autoresponsiveProps: this.getAutoresponsiveProps(),
+      gridHeight: 10
+    };
+  },
+  componentWillReceiveProps(nextProps) {
+    var modules = this.getModules(this.props.modules)
+    this.setState({modules: modules})
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('resize', this.onResize, false);
+  },
+  onResize: function() {
+    this.setState({autoresponsiveProps: this.getAutoresponsiveProps()});
+  },
+
+  render: function () {
+    return (
+      <AutoResponsive {...this.state.autoresponsiveProps} >
+        { this.renderModules() }
+      </AutoResponsive>
+    )
+  },
+  renderModules: function() {
+    return this.state.modules.map((module, i) => {
+        // set width and height, limit width to containerWidth
+        /*var moduleStyle = this.state.moduleStyles[module.key];*/
+        var styles = {
+              width: Math.min(module.width*this.state.autoresponsiveProps.gridWidth, this.state.autoresponsiveProps.containerWidth),
+              height: module.height*this.state.gridHeight
+            }
+        return (
+          <div className="grid-item" key={module.key} style={styles}>
+            <div className="index">00.0{i}.00{module.key}</div>
+            <div className="close" onClick={this.props.do.bind(null,"removeModule",module.key)}>x<div> close</div></div>
+            <Module do={this.props.do} type={module.type} key={module.key} data={module.data} index={i} onHeightChange={this.onModuleHeightChange}/>
+          </div>
+        )
+      })
+  },
+
+  /* Custom Methods */
+  getAutoresponsiveProps: function() {
+    var em = document.body.clientWidth > 659 ? 17 : 15 /* mobile breakpoint at 659px */
+    return {
+      containerWidth: document.body.clientWidth-1*em, /* 1em border-width */
+      gridWidth: em, /* 1em grid-width */
+      itemClassName: 'grid-item',
+      itemMargin: 0,
+      transitionDuration: '.4',
+      transitionTimingFunction: 'ease'
+    }
+  },
+
+  getModules(targetModules) {
+    var results = [];
+    var modules = (this.state && this.state.modules) ? this.state.modules : [];
+    // Update internal state to match modules from props
+    targetModules.forEach((targetModule,i) => {
+      var module = this.findModuleByKey(targetModule.key)
+      if(module) {
+        // add existing module to results
+        results.push(module);
+      }
+      else {
+        // add new module to results
+        results.push( this.createModule(targetModule) )
+      }
+    })
+    // TODO: detect removed modules
+    return results;
+  },
+
+  findModuleByKey(key) {
+    var modules = (this.state && this.state.modules) ? this.state.modules : [];
+    var result
+    modules.some((module) => {
+      if(module.key == key) {
+        result = module
+        return true
+      }
+    })
+    if(result) return result
+    return false
+  },
+
+  createModule(props) {
+    props.width = props.type=="zones" ? 30 : 20
+    props.height = 10
+    return props;
+  },
+
+  onModuleHeightChange: function(index,height) {
+    var modules = this.state.modules
+    // update style of this module to match height of content, plus bottom margin (30px)
+    modules[index].height = Math.ceil(height/this.state.gridHeight) + 3
+    this.setState({modules: modules} )
+  }
+
+});
--- app/components/SqynetApp.jsx
+++ app/components/SqynetApp.jsx
@@ -8,6 +8,7 @@ var AutoResponsive = require('autoresponsive-react');
 var MenuBar = require('./MenuBar.jsx');
 var Module = require('./Module.jsx');
 var Message = require('./Message.jsx');
+var Grid = require('./Grid.jsx');
 
 /* Module, export component */
 module.exports = React.createClass({
@@ -21,20 +22,7 @@ module.exports = React.createClass({
       lastModuleKey: 1,
       messages: [],
       lastMessageKey: 0,
-      autoresponsiveProps: this.getAutoresponsiveProps(),
-      gridHeight: 10
     };
-  },
-  getAutoresponsiveProps: function() {
-    var em = document.body.clientWidth > 659 ? 17 : 15 /* mobile breakpoint at 659px */
-    return {
-      containerWidth: document.body.clientWidth-1*em, /* 1em border-width */
-      gridWidth: em, /* 1em grid-width */
-      itemClassName: 'grid-item',
-      itemMargin: 0,
-      transitionDuration: '.4',
-      transitionTimingFunction: 'ease'
-    }
   },
   getInitialModules: function() {
     if( cookie.load('hideWelcome') ) {
@@ -44,13 +32,9 @@ module.exports = React.createClass({
       return [ ]
     }
   },
+
   componentDidMount: function() {
     this.apiGetAccount()
-
-    window.addEventListener('resize', this.onResize, false);
-  },
-  onResize: function() {
-    this.setState({autoresponsiveProps: this.getAutoresponsiveProps()});
   },
 
   render: function () {
@@ -66,31 +50,14 @@ module.exports = React.createClass({
           }
           </ReactCSSTransitionGroup>
         </section>
-        <AutoResponsive {...this.state.autoresponsiveProps} >
+        <Grid do={this.do} modules={this.state.modules} />
+        { /*<AutoResponsive {...this.state.autoresponsiveProps} >
           { this.renderModules() }
-        </AutoResponsive>
+        </AutoResponsive> */ }
         <Module type="addmodule" do={this.do} />
         <div className="background"></div>
       </div>
     );
-  },
-  renderModules: function() {
-    return this.state.modules.map((module, i) => {
-        // set width and height, limit width to containerWidth
-        var styles = {
-              width: Math.min(module.width*this.state.autoresponsiveProps.gridWidth, this.state.autoresponsiveProps.containerWidth),
-              height: module.height*this.state.gridHeight
-            }
-        return (
-          <div className="grid-item" key={module.key} style={styles}>
-            <div className="index">00.0{i}.00{module.key}</div>
-            <div className="close" onClick={this.removeModule.bind(null,i)}>x<div> close</div></div>
-              <ReactCSSTransitionGroup transitionName="slideInOut" transitionAppear={true}>
-                <Module do={this.do} type={module.type} key={module.key} data={module.data} index={i} onHeightChange={this.onModuleHeightChange}/>
-              </ReactCSSTransitionGroup>
-          </div>
-        )
-      })
   },
 
   /* Custom Methods */
@@ -107,11 +74,13 @@ module.exports = React.createClass({
       case "focusLogin": this.focusLogin(argument); break;
     }
   },
+
   apiGetAccount: function() {
     helpers.getAPI('getAccount')
       .then(this.onGetAccountResponse)
-      .catch(this.onParsingFail)
+      .catch(this.onFail)
   },
+
   onGetAccountResponse: function(json) {
     if(json.error) {
       // Not logged in
@@ -119,17 +88,20 @@ module.exports = React.createClass({
       this.setState({user: json.user})
     }
   },
+
   focusLogin: function() {
     // Set loginFocus to true for a short period of time
     // to trigger focus in Account.jsx
     setTimeout(() =>{ this.setState({loginFocus: false}) }, 1000)
     this.setState({loginFocus: true})
   },
+
   onUsernameChange: function(username) {
     var user = this.state.user;
     user.Username = username;
     this.setState({user: user});
   },
+
   onRegister: function(username) {
     console.log("onRegister "+username);
     if(this.state.user.ID==null) {
@@ -137,12 +109,28 @@ module.exports = React.createClass({
       this.focusLogin()
     }
   },
+
   onLogin: function(user) {
     this.setState({user: user});
   },
+
   onLogout: function() {
     this.setState({user: {ID: null, Username: ""}});
   },
+
+  createModule: function(args) {
+    var type = args.type
+    var data = args.data
+    var key = this.state ? this.state.lastModuleKey+1 : 0;
+    return {
+      key: key,
+      type: type,
+      /*width: type=="zones" ? 30 : 20,
+      height: 10,*/
+      data: data
+    }
+  },
+
   addModule: function(args) {
     var modules = this.state.modules;
     // create and add new module to array
@@ -151,45 +139,36 @@ module.exports = React.createClass({
     // update state with new modules array
     this.setState({modules: modules, lastModuleKey: module.key} )
   },
-  createModule: function(args) {
-    var type = args.type
-    var data = args.data
-    var key = this.state ? this.state.lastModuleKey+1 : 0;
-    return {
-      key: key,
-      type: type,
-      width: type=="zones" ? 30 : 20,
-      height: 10,
-      data: data
-    }
+
+  removeModule: function(key) {
+    var modules = this.state.modules
+    modules.some((module,i) => {
+      if(module.key == key) {
+        // when hiding welcome message, save in cookie to disable showing in future
+        if(modules[i].type=="welcome") cookie.save('hideWelcome', true)
+        // remove module
+        modules.splice(i, 1)
+        this.setState({modules: modules} )
+        return true
+      }
+    })
   },
-  removeModule: function(index) {
-    var modules = this.state.modules;
-    // when hiding welcome message, save in cookie to disable showing in future
-    if(modules[index].type=="welcome") cookie.save('hideWelcome', true);
-    // remove module
-    modules.splice(index, 1);
-    this.setState({modules: modules} )
-  },
+
   addMessage: function(message) {
     var messages = this.state.messages
     var key = this.state.lastMessageKey+1
     messages.push({key: key, type: message.type, content: message.content})
     this.setState({messages: messages, lastMessageKey: key} )
   },
+
   removeMessage: function(index) {
     var messages = this.state.messages;
     messages.splice(index, 1);
     this.setState({messages: messages} )
   },
-  onParsingFail: function(ex) {
-    console.log('JSON parsing failed', ex)
+
+  onFail: function(ex) {
+    console.log('JSON parsing or handling failed', ex)
   },
-  onModuleHeightChange: function(index,height) {
-    var modules = this.state.modules;
-    // update style of this module to match height of content, plus bottom margin (30px)
-    modules[index].height = Math.ceil(height/this.state.gridHeight) + 3;
-    this.setState({modules: modules} )
-  }
 
 });
